c389bbd9bcc9556656b4205c2617efcef8aa8dbd
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-19 12:37:53 +0200

    update to JavaScript file created

--- M2-W3-P2.js
+++ M2-W3-P2.js
@@ -23,3 +23,24 @@ const toggleImagesClass = function () {
       singleImg.classList.toggle("hidden") 
     }
 }
+function randomPriceColor() {
+    let changePriceColor = document.getElementsByClassName ("price") 
+   let randomColor1 = (Math.floor(Math.random()*(256 - 0) +  0))
+   let randomColor2 = (Math.floor(Math.random()*(256 - 0) +  0))
+   let randomColor3 = (Math.floor(Math.random()*(256 - 0) +  0))
+    var year = 25
+    console.log("I am year old"); 
+    console.log(`I am ${year} old`);
+   //    randomColor = randomColor.toString
+    for (let i = 0; i < changePriceColor.length; i++) {
+        const singlePrice = changePriceColor[i];
+     //    singlePrice.style.color = "rgb(234, 234, 234)"
+         //  singlePrice.style.color = "rgb("+randomColor1 + ","+randomColor2 + ","+randomColor3 + ")"
+         
+           singlePrice.style.color = `rgb(${randomColor1},${randomColor2},${randomColor3})`
+    } 
+   //    randomColor = randomColor.toString 
+    
+   }
+   randomPriceColor()
+   // singlePrice.style.color = `rgb (${randomColor},${randomColor},${randomColor})`
