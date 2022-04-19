18cdfcd0a0859af9022dd20eae92d3d001bfcf78
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-04-19 12:40:24 +0200

    solved exercise 12

--- M2-W3-P2.js
+++ M2-W3-P2.js
@@ -28,19 +28,10 @@ function randomPriceColor() {
    let randomColor1 = (Math.floor(Math.random()*(256 - 0) +  0))
    let randomColor2 = (Math.floor(Math.random()*(256 - 0) +  0))
    let randomColor3 = (Math.floor(Math.random()*(256 - 0) +  0))
-    var year = 25
-    console.log("I am year old"); 
-    console.log(`I am ${year} old`);
-   //    randomColor = randomColor.toString
-    for (let i = 0; i < changePriceColor.length; i++) {
-        const singlePrice = changePriceColor[i];
-     //    singlePrice.style.color = "rgb(234, 234, 234)"
-         //  singlePrice.style.color = "rgb("+randomColor1 + ","+randomColor2 + ","+randomColor3 + ")"
-         
-           singlePrice.style.color = `rgb(${randomColor1},${randomColor2},${randomColor3})`
+  for (let i = 0; i < changePriceColor.length; i++) {
+        const singlePrice = changePriceColor[i]; 
+        singlePrice.style.color = `rgb(${randomColor1},${randomColor2},${randomColor3})`
     } 
-   //    randomColor = randomColor.toString 
-    
-   }
-   randomPriceColor()
-   // singlePrice.style.color = `rgb (${randomColor},${randomColor},${randomColor})`
+}
+randomPriceColor()
+  
